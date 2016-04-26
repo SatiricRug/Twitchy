6eb743fea88e56421db93043856cd7ad10bc24ec
Peter <pjschinske@gmail.com>
2016-04-25 17:42:49 -0700

    Reorganized, added documentation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import java.io.File;
 import java.util.ArrayList;
 
 /*
@@ -13,16 +14,19 @@ import java.util.ArrayList;
 
 public class Main {
 	
-	public static String streamer;
-	public static String quality;
+	public static String streamer = "food";
+	public static String quality = "Low";
 	
 	public static void main(String[] args) throws Exception {
-		TwitchAPI.requestToken("food");
-		ArrayList<Stream> streams = TwitchAPI.requestStreams();
+		TwitchAPIv1.requestToken(streamer);
+		File file = TwitchAPIv1.requestStreams();
+		ArrayList<Stream> streams = TwitchAPIv1.parseStreamsList(file);
+		
 		for (Stream stream: streams) {
 			String streamQuality = stream.getStreamQuality();
-			if (streamQuality.equals("Low")) {
+			if (streamQuality.equals(quality)) {
 				Runtime rt = Runtime.getRuntime();
+				@SuppressWarnings("unused")
 				Process pr = rt.exec("C:\\Users\\pjsch_000\\Desktop\\vlc-2.1.5\\vlc.exe " + stream.getStreamURL().toString());
 			}
 		}
