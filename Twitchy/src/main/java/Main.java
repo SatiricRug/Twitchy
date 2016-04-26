b84b6cb07714cbd3c9c9a8d6b1485b0e17e6c8dc
Peter <pjschinske@gmail.com>
2016-04-25 17:40:32 -0700

    Reorganized, added documentation

---
+++ Twitchy/src/main/java/Main.java
@@ -0,0 +1,35 @@
+package main.java;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/*
+ * TODO:
+ * Authenticate w/ Twitch API
+ * Access followed streamers that are currently streaming
+ * Input streamer and stream quality
+ * output stream
+ * output chat
+ */
+
+public class Main {
+	
+	public static String streamer = "food";
+	public static String quality = "Low";
+	
+	public static void main(String[] args) throws Exception {
+		TwitchAPIv1.requestToken(streamer);
+		File file = TwitchAPIv1.requestStreams();
+		ArrayList<Stream> streams = TwitchAPIv1.parseStreamsList(file);
+		
+		for (Stream stream: streams) {
+			String streamQuality = stream.getStreamQuality();
+			if (streamQuality.equals(quality)) {
+				Runtime rt = Runtime.getRuntime();
+				@SuppressWarnings("unused")
+				Process pr = rt.exec("C:\\Users\\pjsch_000\\Desktop\\vlc-2.1.5\\vlc.exe " + stream.getStreamURL().toString());
+			}
+		}
+	}
+	
+}
