3877cc13b044d0fce2384fb7258805e43473e455
Peter <pjschinske@gmail.com>
2016-04-27 18:06:54 -0700

    Started to work on GUI

--- src/main/java/gui/Gui.java
+++ src/main/java/gui/Gui.java
@@ -1,26 +1,38 @@
 package main.java.gui;
 
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Gui {
 	
-	public static JFrame frame;
-	public static JPanel panel;
-	public static JLabel labelStreamerName;
-	public static JLabel labelStreamQuality;
+	private static JFrame frame;
+	private static JPanel panel;
+	private static JLabel labelStreamerName;
+	private static JLabel labelStreamQuality;
+	private static JTextField enterStreamerName;
+	private static JTextField enterStreamQuality;
 	
-	public static final int WIDTH = 400;
+	public static final int WIDTH = 152;
 	public static final int HEIGHT = 300;
 	
-	public static void createGui() {
+	public static void createGui() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		int widthMonitor = gd.getDisplayMode().getWidth();
+		int heightMonitor = gd.getDisplayMode().getHeight();
+		
 		frame = new JFrame("Twitchy");
-		frame.setBounds(1920 / 2 - WIDTH / 2, 1080 / 2 - HEIGHT / 2, WIDTH, HEIGHT);
+		frame.setBounds(widthMonitor / 2 - WIDTH / 2, heightMonitor / 2 - HEIGHT / 2, WIDTH, HEIGHT);
 		frame.setResizable(false);
 		
 		frame.addWindowListener(new WindowAdapter() {
@@ -29,14 +41,28 @@ public class Gui {
 	    	}
 	    });
 		
-		panel = new JPanel();
+		panel = new JPanel(null);
 		
-		labelStreamerName = new JLabel("Streamer Name:", SwingConstants.LEFT);
-		labelStreamerName.setBounds(100, 100, 100, 100);
-		labelStreamQuality = new JLabel("Stream Quality:", SwingConstants.LEFT);
+		labelStreamerName = new JLabel("Streamer Name:");
+		labelStreamerName.setLocation(10, 75);
+		labelStreamerName.setSize(labelStreamerName.getPreferredSize());
 		
+		enterStreamerName = new JTextField(15);
+		enterStreamerName.setLocation(10, 95);
+		enterStreamerName.setSize(enterStreamerName.getPreferredSize());
+		
+		labelStreamQuality = new JLabel("Stream Quality:");
+		labelStreamQuality.setLocation(10, 125);
+		labelStreamQuality.setSize(labelStreamQuality.getPreferredSize());
+		
+		enterStreamQuality = new JTextField(15);
+		enterStreamQuality.setLocation(10, 145);
+		enterStreamQuality.setSize(enterStreamQuality.getPreferredSize());
+		System.out.println(enterStreamQuality.getPreferredSize().getWidth());
 		panel.add(labelStreamerName);
-//		panel.add(labelStreamQuality);
+		panel.add(enterStreamerName);
+		panel.add(labelStreamQuality);
+		panel.add(enterStreamQuality);
 		frame.add(panel);
 		
 		frame.setVisible(true);
